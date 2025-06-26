e6c9b0d802fe32aae0670337bbf463332eaeaca8
miggy-py <mperezrojo23@gmail.com>
2025-06-26 14:19:25 -0400

    Established career data

--- project.py
+++ project.py
@@ -1,5 +1,17 @@
+from player_util import start_player_csv
+from player import Player
+from stats import get_player_stats
+
+
 def main():
-    ...
+    start_player_csv()
+
+    my_player : Player = Player("Lebron James")
+
+    career = get_player_stats(my_player)
+
+    print(career)
+
 
 
 if __name__ == "__main__":
